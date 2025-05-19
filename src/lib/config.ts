76ff5641271733987499eefbcbc27f23ddf6109b
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2025-05-19 13:09:04 +0800

    fix: added render domain on config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,3 @@
 export default {
-    SERVER: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5000' : 'https://our-domain.here'
+    SERVER: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5000' : 'https://nlp-finals.onrender.com'
 }
